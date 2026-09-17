472ecc0ad8ba7ff7df25d6ac6a346cb21cc5994c
Curt N Rode <0expatriate0@gmail.com>
2026-09-17 17:14:59 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let today = new Date().toLocaleDateString();
-console.log(today);                    
+console.log(today);
 
 let myFavorite = {
     name: 'Buff Bros',
@@ -9,13 +9,13 @@ let myFavorite = {
     dateAdded: today
 };
 
-console.log(myFavorite);                 
-console.log(typeof myFavorite.name);     
-console.log(typeof myFavorite.rating);  
+console.log(myFavorite);
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.rating);
 
-console.log(typeof myFavorite.category);  
-console.log(typeof myFavorite.notes);    
-console.log(typeof myFavorite.dateAdded);  
+console.log(typeof myFavorite.category);
+console.log(typeof myFavorite.notes);
+console.log(typeof myFavorite.dateAdded);
 
 console.log(myFavorite.name);
 let displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
@@ -23,3 +23,22 @@ let displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
 console.log(displayText);
 
 console.log('⭐'.repeat(myFavorite.rating) + ' ' + myFavorite.name);
+
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + ' has ' + rating + ' stars!');
+}
+greetFavorite('Starbucks', 5);   // "Starbucks has 5 stars!"
+
+
+const nameInput = document.getElementById('name');
+console.log(nameInput.value);   // what the user typed
+
+const practiceForm = document.getElementById('add-favorite-form');
+
+function handleSubmit(event) {
+    event.preventDefault();   // stop the page reload
+    console.log('You typed: ' + nameInput.value);
+}
+
+practiceForm.addEventListener('submit', handleSubmit);
